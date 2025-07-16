0029f94a6e305eb9214cc3670b8cb84d2755dd47
Fahadkhan1503 <fahadkhan1503@gmail.com>
2025-07-16 14:48:10 +0500

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connect } from 'mongoose';
+import connectDb from './database/db.js';
 
+dotenv.config();
 const app = express();
 
-app.listen(5000, () => {
-    console.log(`Server is working on port 5000`);
-})
+app.listen(process.env.PORT, () => {
+    console.log(`Server is working on port ${process.env.PORT}`);
+    connectDb()
+});
+
