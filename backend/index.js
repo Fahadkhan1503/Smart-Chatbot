242eaee9cb3df3f9d482da88eed3863f85524a69
hamza0426 <muhammadhamzaowais5@gmail.com>
2025-07-18 00:11:06 +0500

    Backend Completed

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 // import { connect } from 'mongoose';
 import connectDb from "./database/db.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
 // using middlewares
 app.use(express.json());
+app.use(cors());
 
 //importing routes
 import userRoutes from "./routes/userRoutes.js";
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,10 +1,19 @@
-import express from 'express';
-import { isAuth } from '../middlewares/isAuth.js';
-import { addConversation, createChat, getAllChats } from '../controllers/chatControllers.js';
+import express from "express";
+import { isAuth } from "../middlewares/isAuth.js";
+import {
+  addConversation,
+  createChat,
+  deletChat,
+  getAllChats,
+  getConversation,
+} from "../controllers/chatControllers.js";
 
 const router = express.Router();
 
-router.post("/new", isAuth, createChat)
+router.post("/new", isAuth, createChat);
 router.get("/all", isAuth, getAllChats);
-router.post("/:id",isAuth,addConversation);
+router.post("/:id", isAuth, addConversation);
+router.get("/:id", isAuth, getConversation);
+router.delete("/:id", isAuth, deletChat);
+
 export default router;
